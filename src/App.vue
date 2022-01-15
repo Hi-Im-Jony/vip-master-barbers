d4868b8512bc50b46a62c012bb310b39c4ecb4de
Hi-Im-Jony <jccicai@yahoo.ie>
2022-01-15 22:52:31 +0000

    actually using router view now

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,17 @@
   <v-app>
     <navbar v-if="windowWidth > 770" />
     <navbar-mobile v-if="windowWidth <= 770" />
-    <home />
+    <router-view />
   </v-app>
 </template>
 
 <script>
-import Home from "./views/Home.vue";
 import Navbar from "./components/navbar.vue";
 import NavbarMobile from "./components/navbar-mobile.vue";
 
 export default {
   name: "App",
-  components: { Navbar, Home, NavbarMobile },
+  components: { Navbar, NavbarMobile },
   data: () => ({
     windowWidth: window.innerWidth,
   }),
