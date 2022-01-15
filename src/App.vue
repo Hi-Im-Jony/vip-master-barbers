a7724357fd0cf132d0ff629067f548a1da183b80
Hi-Im-Jony <jccicai@yahoo.ie>
2022-01-15 21:20:21 +0000

    renamed component more appropiately

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
   <v-app>
     <navbar v-if="windowWidth > 770" />
-    <nav-two v-if="windowWidth <= 770" />
+    <navbar-mobile v-if="windowWidth <= 770" />
     <home />
   </v-app>
 </template>
 
 <script>
-import NavTwo from "@/components/nav-two.vue";
 import Home from "./views/Home.vue";
 import Navbar from "./components/navbar.vue";
+import NavbarMobile from "./components/navbar-mobile.vue";
 
 export default {
   name: "App",
-  components: { Navbar, NavTwo, Home },
+  components: { Navbar, Home, NavbarMobile },
   data: () => ({
     windowWidth: window.innerWidth,
   }),
