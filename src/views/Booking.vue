6071509e6004ee3c8d89ff7f6f4ab00a47f7d5fa
Hi-Im-Jony <jccicai@yahoo.ie>
2022-01-17 12:12:42 +0000

    started on a booking calendar

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,13 +1,27 @@
 <template>
   <div id="booking-page">
-    <h1>hello</h1>
+    <h1>Make a Booking</h1>
+    <booking-calendar />
   </div>
 </template>
 
 <script>
+import BookingCalendar from "../components/BookingCalendar.vue";
 export default {
+  components: { BookingCalendar },
   name: "Booking",
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#booking-page {
+  min-height: 100vh;
+  background-color: rgb(30, 30, 32);
+  padding: 70px 0 0 0;
+  text-align: center;
+  color: whitesmoke;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
