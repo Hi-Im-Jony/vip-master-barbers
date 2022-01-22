c9906839cddef3c1a32d96bf99f8ecb3c02da8b7
Hi-Im-Jony <jccicai@yahoo.ie>
2022-01-22 22:09:15 +0000

    removed an un-needed wrapper component

--- src/views/Booking.vue
+++ src/views/Booking.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="booking-page">
-    <h1>Make a Booking</h1>
-    <booking-calendar />
+    <h1 id="page-title">Make a Booking</h1>
+    <barber-selector v-model="selectedBarber" />
+    <p @click="print(selectedBarber)">View Barber</p>
+
+    <calendar v-model="selectedDate" />
+    <p @click="print(selectedDate)">View Date</p>
   </div>
 </template>
 
 <script>
-import BookingCalendar from "../components/BookingCalendar.vue";
+import BarberSelector from "@/components/BarberSelector.vue";
+import Calendar from "@/components/Calendar.vue";
 export default {
-  components: { BookingCalendar },
-  name: "Booking",
+  components: { BarberSelector, Calendar },
+  data() {
+    return {
+      selectedBarber: null,
+      selectedDate: null,
+    };
+  },
+  methods: {
+    print: function(p) {
+      console.log(p);
+    },
+  },
 };
 </script>
 
@@ -18,10 +33,13 @@ export default {
   min-height: 100vh;
   background-color: rgb(30, 30, 32);
   padding: 70px 0 0 0;
-  text-align: center;
   color: whitesmoke;
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+#page-title {
+  width: 100%;
+  text-align: center;
 }
 </style>
