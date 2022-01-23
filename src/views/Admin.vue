327187ca16138326c7173e5a7a58e979d5f0f6d0
Hi-Im-Jony <jccicai@yahoo.ie>
2022-01-23 14:58:38 +0000

    started work on admin page

---
+++ src/views/Admin.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="admin-page">
+    <div>
+      <h1>Add a Barber</h1>
+      <v-text-field dark v-model="bName" label="Barber Name"></v-text-field>
+      <a @click="createBarber(bName)">Add</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as fb from "@/fb";
+export default {
+  data() {
+    return {
+      bName: "",
+    };
+  },
+  methods: {
+    print: function(p) {
+      console.log(p);
+    },
+    createBarber: async function(barberName) {
+      await fb.createBarber(barberName);
+    },
+  },
+};
+</script>
+
+<style scoped>
+#admin-page {
+  min-height: 100vh;
+  background-color: rgb(30, 30, 32);
+  padding: 70px 0 0 0;
+  color: whitesmoke;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
