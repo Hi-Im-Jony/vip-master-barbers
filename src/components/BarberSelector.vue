81d9a396f64669e07f71061b83c8551666893a70
Hi-Im-Jony <jccicai@yahoo.ie>
2022-01-23 17:58:25 +0000

    only one barber can be selected

--- src/components/BarberSelector.vue
+++ src/components/BarberSelector.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="barber-selector">
     <h2>Select a Barber</h2>
-    <div v-for="barber in barbers" :key="barber">
-      <v-checkbox
-        dark
-        :created="addBarberSel(barber)"
-        :label="barber"
-        v-model="checkboxes[barber]"
-        @click="update()"
+    <div class="barber" v-for="barber in barbers" :key="barber">
+      <div :class="getIndicatorClass(barber)"></div>
+      <input
+        style="display:none"
+        type="radio"
+        name="barber"
+        :id="barber"
+        @click="update(barber)"
       />
+      <label :for="barber" class="barber-name">{{ barber }}</label>
     </div>
   </div>
 </template>
@@ -18,8 +20,8 @@ import * as fb from "@/fb";
 export default {
   data() {
     return {
-      checkboxes: [],
       barbers: [],
+      selectedBarber: "",
     };
   },
   created: function() {
@@ -31,17 +33,35 @@ export default {
         this.barbers = response;
       });
     },
-    addBarberSel: function(barber) {
-      this.checkboxes[barber] = false;
+    getIndicatorClass: function(barber) {
+      if (this.selectedBarber === barber) return "indicator selected";
+      else return "indicator ";
     },
-    update: function() {
-      const checkboxes = this.checkboxes;
-      let selectedBarbers = [];
-      for (let barber in checkboxes) {
-        if (checkboxes[barber]) selectedBarbers.push(barber);
-      }
-      this.$emit("input", selectedBarbers);
+    update: function(barber) {
+      this.selectedBarber = barber;
+      let selectedBarber = [barber];
+      this.$emit("input", selectedBarber);
     },
   },
 };
 </script>
+
+<style scoped>
+.barber {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+.barber-name {
+  margin: 6px !important;
+  font-size: 25px;
+}
+.indicator {
+  width: 25px;
+  height: 25px;
+  border: solid aliceblue;
+}
+.selected {
+  background: gainsboro;
+}
+</style>
