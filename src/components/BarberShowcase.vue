7a8584aa98aa5eb4bd5556cabad1d6c4b0e32368
Hi-Im-Jony <jccicai@yahoo.ie>
2022-04-02 15:30:21 +0100

    added two barber-showcases

---
+++ src/components/BarberShowcase.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="barber-showcase">
+    <h4>{{ name }}</h4>
+    <img :src="require('@/assets/' + src + '')" />
+    <p>{{ description }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name", "description", "src"],
+};
+</script>
+
+<style scoped>
+img {
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+  object-position: 100% 0;
+}
+</style>
--- src/components/TheTeamSection.vue
+++ src/components/TheTeamSection.vue
@@ -1,11 +1,37 @@
 <template>
   <section id="the-team">
     <h2 id="section-title">Meet The Team</h2>
+    <barber-showcase
+      name="Elvis"
+      description="A description"
+      src="elvis.jpeg"
+    />
+    <barber-showcase
+      name="Gabrila"
+      description="A description"
+      src="gab.jpeg"
+    />
   </section>
 </template>
 
 <script>
-export default {};
+import * as fb from "@/fb";
+import BarberShowcase from "./BarberShowcase.vue";
+
+export default {
+  components: { BarberShowcase },
+  data() {
+    return {
+      barbers: [],
+    };
+  },
+  created: async function() {
+    await fb.getAllBarbers().then((response) => {
+      this.barbers = response;
+    });
+    this.getAttempted = true;
+  },
+};
 </script>
 
 <style scoped>
