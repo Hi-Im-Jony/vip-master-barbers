0cfbce4eec5dc6e458c8a35e7f315e9e07428a28
Hi-Im-Jony <jccicai@yahoo.ie>
2022-04-02 18:44:55 +0100

    added some intro text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,9 +6,22 @@
       description="Where Everyone Is A VIP"
       image="saloon3-mbl.jpg"
     />
-    <div class="section-spacer" style="height:20px"></div>
-    <services-section />
+    <h2 id="intro-title">About Us</h2>
+    <div id="intro-text">
+      <p>
+        Elvis Hairstyle is a luxurious barber shop that delivers awesome
+        haircuts and barbering services to customers.
+      </p>
+      <p>
+        Our fully equipped shop has the most recent barbering or haircut machine
+        that ensures your world-class experience for our customers. Our main aim
+        has always been in developing a great fusion of light, music,
+        uniqueness, and remarkable barbering services that surely offer and
+        customers a mind-blowing experience.
+      </p>
+    </div>
     <the-team-section />
+    <services-section />
     <map-section />
   </div>
 </template>
@@ -37,13 +50,18 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 60px 0 0 0;
+  padding: 0px 0 0 0;
   background-color: white;
 }
-
-@media (max-width: 860px) {
-  #homepage {
-    padding: 0px 0 0 0;
-  }
+#intro-title {
+  text-decoration: underline;
+  color: rgb(26, 45, 59);
+  font-size: 27px;
+  text-align: center;
+}
+#intro-text {
+  padding: 10px;
+  text-align: center;
+  max-width: 300px;
 }
 </style>
