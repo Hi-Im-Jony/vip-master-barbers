3c399479b144845f817725cfe28407b4494f3610
Hi-Im-Jony <jccicai@yahoo.ie>
2022-04-03 14:45:03 +0100

    added some text to team section

--- src/components/TheTeamSection.vue
+++ src/components/TheTeamSection.vue
@@ -6,11 +6,32 @@
       description="Professional Hairstylist"
       src="elvis.jpeg"
     />
+    <p>
+      With over 15 years of experience in servicing clients' hair and barber
+      needs, Elvis has seen and done it all. His expertise, though, lies in
+      men's haircuts and styling.
+    </p>
+    <p>
+      His attention to detail and intense passion in providing exceptional
+      barber experience has led him to provide clients with high-quality hair
+      services.
+    </p>
     <barber-showcase
       name="Gabrila"
       description="Professional Hairstylist"
       src="gab.jpeg"
     />
+    <p>
+      Gabrila has been in the Salon Industry for over 15 years. She specializes
+      in a variety of men's haircuts. Gabrila is a perfectionist in everything
+      that she does. She always has a smile on her face and greets everyone
+      welcomely.
+    </p>
+    <p>
+      She treats everyone with the best hospitality she will always offer you a
+      glass of water or a cup of coffee to make you feel welcome as you wait for
+      your service.
+    </p>
   </section>
 </template>
 
@@ -39,5 +60,11 @@ export default {
   color: rgb(26, 45, 59);
   font-size: 27px;
   text-align: center;
+}
+p {
+  width: 85vw;
+  max-width: 600px;
+  text-align: center;
+  margin: 5px 0 !important;
 }
 </style>
